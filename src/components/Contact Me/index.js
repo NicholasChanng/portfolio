c2041b2df135f4361b1e38b7e2e88688ad5698d8
Nicholas Channg <23nichanng@gmail.com>
2023-12-02 19:34:40 -0500

    V_1.4
    Added contact fill in form (does not send email)

--- src/components/Contact Me/index.js
+++ src/components/Contact Me/index.js
@@ -4,11 +4,34 @@ import './index.scss';
 const Contact = () =>{
     return(
         <>
-        <div className= "container">
-            <h1 className="header">
-                Contact Me
-            </h1>
-
+        <div className= "container contact-page">
+            <div className="text-zone">
+                <h1>
+                    Contact Me
+                </h1>
+                <p>
+                    I am actively seeking internships and jobs in the tech field, <br/>
+                    but any opportunties are greatly appreciated. Please feel free <br/>
+                    to reach out using the form below. 
+                </p>
+                <form>
+                    <li className="half">
+                        <input type = "text" name = "name" placeholder="Name" required/>
+                    </li>
+                    <li className="half">
+                        <input type = "email" name = "email" placeholder="Email" required/>
+                    </li>
+                    <li>
+                        <input type = "text" name= "subject" placeholder="Subject" required/>
+                    </li>
+                    <li>
+                        <textarea name="message" placeholder="Message" required/>
+                    </li>
+                    <li>
+                        <input type="submit" className="flat-button" value="SEND" />
+                    </li>
+                </form>
+            </div>
         </div>
         <Loader type="ball-pulse-sync" />
         </>
