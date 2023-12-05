ad5e1354a100bbf863c0ee3739eb4fb9c06c1f3a
Nicholas Channg <23nichanng@gmail.com>
2023-12-04 22:25:22 -0500

    V_1.6
    Cleaned side bar's css and add added hover effect to menu.

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -4,15 +4,17 @@ import './index.scss';
 const Experience = () =>{
     return(
         <>
-        <div className="container">
-            <h1 className= "header">
-                Experience
-            </h1>
+        <div className="container experience-page">
+            <div className='text-zone'>
+                <h1>
+                    Experience
+                </h1>
             <ul>
                 <li>
-
+                    
                 </li>
             </ul>
+            </div>
         </div>
         <Loader type="ball-pulse-sync" />
         </>
