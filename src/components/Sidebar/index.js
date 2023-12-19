2e57d0a26584c58a525400f980f6035445d3c710
Nicholas Channg <nicholas.channg@gmail.com>
2023-12-19 07:23:27 -0500

    2.1
    Fixed logo click to #home.

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,41 +1,34 @@
-import './index.scss';
-import LogoS from '../../assets/images/FlamingLogo.png';
-import {Link} from 'react-router-dom';
+import './index.scss'
+import LogoS from '../../assets/images/FlamingLogo.png'
 import Menu from '../Menu'
 
-
-
 const Sidebar = () => (
-  <div className='nav-bar'>
-    <div className='nav-bar-page'>
-      <div className = "content-area">
-        <Link className="logo" to = "/">
-            <img src={LogoS} alt = "logo"/>
-            <h1 className='sub-logo'>Nicholas Channg</h1>
-        </Link>
-        <div className='link-area'>
-          <nav className='links'>
-            <a href = "#home">
-              Home
-            </a>
+  <div className="nav-bar">
+    <div className="nav-bar-page">
+      <div className="content-area">
+        <div className="logo">
+          <a href="#home">
+            <img src={LogoS} alt="logo" />
+          </a>
+          <a href="#home">
+            <h1 className="sub-logo">Nicholas Channg</h1>
+          </a>
+        </div>
+        <div className="link-area">
+          <nav className="links">
+            <a href="#home">Home</a>
 
-            <a href = "#education">
-            Education
-            </a>
-        
-            <a href = "#portfolio">
-            Portfolio
-            </a>
-        
-            <a href = "#contact">
-            Contact
-            </a>
+            <a href="#education">Education</a>
+
+            <a href="#portfolio">Portfolio</a>
+
+            <a href="#contact">Contact</a>
           </nav>
-          <Menu/>
+          <Menu />
         </div>
       </div>
     </div>
   </div>
- )
+)
 
-export default Sidebar;
+export default Sidebar
