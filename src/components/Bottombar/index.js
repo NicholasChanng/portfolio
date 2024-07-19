86001bec447001e293b344ff45eebebfb7592172
NicholasChanng <nicholas.channg@gmail.com>
2024-07-19 18:55:04 -0400

    Changed resume

--- src/components/Bottombar/index.js
+++ src/components/Bottombar/index.js
@@ -47,7 +47,7 @@ const Bottombar = () => {
               </Link>
               <Link
                 className="social-button"
-                to="https://docs.google.com/document/d/1X9eCtOV4b3hAHcveuq3-ebRm1y5h25T66c6AxbJhwh0/edit?usp=sharing"
+                to="https://docs.google.com/document/d/16CVvyqrcSoxm_TymApA0ifMq28jLlMgsrhmYsqQ2ukU/edit?usp=sharing"
                 target="_blank"
                 rel="noreferrer"
               >
